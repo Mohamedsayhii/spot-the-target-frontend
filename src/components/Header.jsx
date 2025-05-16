ae0b0a69b41f52cc688e2f1c65fb572ebd756123
Mohamed Sayhi <mohamedsayhii@outlook.com>
2025-05-16 23:10:56 +0100

    Redesign the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Navbar = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	.nav-buttons {
+		display: flex;
+		gap: 2rem;
+	}
+`;
+
 function Header() {
 	return (
-		<>
+		<Navbar>
 			<h1>Spot The Target</h1>
-		</>
+			<div className='nav-buttons'>
+				<h1>HOME</h1>
+				<h1>LEADERBOARDS</h1>
+			</div>
+		</Navbar>
 	);
 }
 
